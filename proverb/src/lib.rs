31c56acaa8c7c24b9fa0bdf50cd2846ed9e430f2
Cesar D. Velandia <cesar@velandia.co>
2020-05-01 20:42:48 +0800

    Complete proverb

---
+++ proverb/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn build_proverb(list: &[&str]) -> String {
+    let mut proverb = String::new();
+    for i in 0..list.len() {
+        if i + 1 < list.len() {
+            proverb.push_str(&*format!(
+                "For want of a {} the {} was lost.\n",
+                list[i],
+                list[i + 1]
+            ));
+        } else {
+            proverb.push_str(&*format!("And all for the want of a {}.", list[0]));
+        }
+    }
+
+    proverb
+}
