c25e87c145ffca96b9dd5361bf924fec3e596d76
Cesar D. Velandia <cesar@velandia.co>
2020-05-08 14:16:48 +0800

    Add sum of multiples

---
+++ sum-of-multiples/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    let mut multiples: Vec<u32> = Vec::new();
+
+    for f in factors {
+        if f > &(0 as u32) {
+            multiples.append(
+                &mut (1..limit)
+                    .filter(|x| x % f == 0 && !multiples.contains(x))
+                    .collect::<Vec<u32>>(),
+            );
+        }
+    }
+
+    println!(
+        "limit={:?} factors={:?}  result={:?}",
+        limit, factors, multiples
+    );
+
+    multiples.into_iter().sum()
+}
---
+++ sum-of-multiples/src/main.rs
@@ -0,0 +1,6 @@
+use sum_of_multiples::*;
+
+pub fn main(){
+    let xs = sum_of_multiples(4, &[3, 0]);
+    println!("{:?}", xs);
+}
