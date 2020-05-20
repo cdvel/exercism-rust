5011a8686814c205153729f10a1499645b111a61
Cesar D. Velandia <cesar@velandia.co>
2020-05-20 16:28:18 +0800

    Add high scores

---
+++ high-scores/src/lib.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        Self { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        match self.scores.last() {
+            Some(x) => Some(*x),
+            None => None,
+        }
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().cloned()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut top = self.scores.to_owned();
+        top.sort();
+        top.into_iter().rev().take(3).collect()
+    }
+}
